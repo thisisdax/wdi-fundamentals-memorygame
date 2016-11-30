d4ec2dcd286e2d86347d331ef15b3665c178ed89
Dax <Dax@Pokedax.local>
2016-11-30 12:42:07 +0800

    added Unit 9 -Game Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+if (cardOne === cardTwo) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again!");
+}
