675ea2e61e530c9d4f50008a13152f9ad1e4b01d
Dax <Dax@Pokedax.local>
2016-12-02 00:09:25 +0800

    unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,20 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
 
-if (cardOne === cardTwo) {
+/* if (cardOne === cardTwo) {
   alert("You found a match!");
 } else {
   alert("Sorry, try again!");
-}
+} */
+
+var gameboard = document.getElementById('game-board');
+
+var createCards = function () {
+  for (var i = 0; i < 4; i++) {
+    var createDiv = document.createElement('div');
+    createDiv.className = 'cards';
+    gameboard.appendChild(createDiv)
+  }
+};
+
+createCards();
