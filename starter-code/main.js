5540ede175ec06b62f751bb751f3274e5f752f6c
Dax <Dax@Pokedax.local>
2016-12-03 02:00:19 +0800

    finalized wdi prework

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,11 @@
 var cards = ['Queen', 'Queen', 'King', 'King'];
 var cardsInPlay = [];
+var scoreCount = 0;
 /* if (cardOne === cardTwo) {
   alert("You found a match!");
 } else {
   alert("Sorry, try again!");
 } */
-
 var gameboard = document.getElementById('game-board');
 
 var createCards = function () {
@@ -19,8 +19,22 @@ var createCards = function () {
   createBtn();
 };
 
-var isTwoCards = function () {
+function shuffle(a) {
+  var currentIndex = a.length, tempValue, randomIndex;
 
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    tempValue = a[currentIndex];
+    a[currentIndex] = a[randomIndex];
+    a[randomIndex] = tempValue;
+  }
+  return a;
+};
+
+var isTwoCards = function () {
     if (this.getAttribute('data-card') === 'King') {
     this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/52/spades-28320_960_720.png' class='cards' alt='King of Spades')/>";
     cardsInPlay.push(this.getAttribute('data-card'));
@@ -30,7 +44,6 @@ var isTwoCards = function () {
     } else {
     alert("Pick another card!");
     }
-    this.setAttribute('data-card', 'chosen');
     if (cardsInPlay.length === 2) {
       isMatch(cardsInPlay);
       cardsInPlay = [];
@@ -39,13 +52,14 @@ var isTwoCards = function () {
 
 var isMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
+    scoreCount++;
     setTimeout(function() {
       alert("You found a match!");
     }, 200);
+    updateScore();
   } else {
     setTimeout(function() {
       alert("Sorry, try again!");
-      reset();
     }, 200);
   }
 };
@@ -58,12 +72,27 @@ var createBtn = function () {
   gameboard.appendChild(resetBtn);
 };
 
+var createScoreBoard = function () {
+  var scoreBoard = document.createElement('div')
+  scoreBoard.setAttribute('id', 'score');
+  scoreBoard.innerHTML = "<p>Your score: </p>" + scoreCount;
+  gameboard.appendChild(scoreBoard);
+};
+
+var updateScore = function () {
+  document.getElementById('score').innerHTML = "<p>Your score: </p>" + scoreCount;
+};
+
 var reset = function () {
-  var gamereset = document.getElementById('game-board');
-  for (var i = gamereset.children.length; i > 0; i--) {
-      gamereset.removeChild(gamereset.children[0]);
+  for (var i = gameboard.children.length; i > 0; i--) {
+      gameboard.removeChild(gameboard.children[0]);
     }
   cardsInPlay = [];
+  scoreCount = 0;
+  createScoreBoard();
+  shuffle(cards);
   createCards();
 };
+createScoreBoard();
+shuffle(cards);
 createCards();
