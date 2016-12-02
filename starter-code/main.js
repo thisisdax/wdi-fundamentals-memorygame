9d8637546a6c1a2bbde9745f36fdd8cc4c10f84b
Dax <Dax@Pokedax.local>
2016-12-03 00:39:38 +0800

    wdi fundamentals prework complete

--- starter-code/main.js
+++ starter-code/main.js
@@ -16,15 +16,21 @@ var createCards = function () {
     createDiv.addEventListener('click', isTwoCards);
     gameboard.appendChild(createDiv);
   }
+  createBtn();
 };
 
 var isTwoCards = function () {
+
     if (this.getAttribute('data-card') === 'King') {
-    this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/52/spades-28320_960_720.png' alt='King of Spades' class='cards') />";
-    } else {
-    this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/48/hearts-28307_960_720.png' alt='Queen of Hearts' class='cards') />";
-    }
+    this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/52/spades-28320_960_720.png' class='cards' alt='King of Spades')/>";
     cardsInPlay.push(this.getAttribute('data-card'));
+    } else if (this.getAttribute('data-card') === 'Queen') {
+    this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/48/hearts-28307_960_720.png' class='cards' alt='Queen of Hearts')/>";
+    cardsInPlay.push(this.getAttribute('data-card'));
+    } else {
+    alert("Pick another card!");
+    }
+    this.setAttribute('data-card', 'chosen');
     if (cardsInPlay.length === 2) {
       isMatch(cardsInPlay);
       cardsInPlay = [];
@@ -33,11 +39,31 @@ var isTwoCards = function () {
 
 var isMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    setTimeout(function() {
+      alert("You found a match!");
+    }, 200);
   } else {
-    alert("Sorry, try again!");
+    setTimeout(function() {
+      alert("Sorry, try again!");
+      reset();
+    }, 200);
   }
-  cardsInPlay
 };
 
+var createBtn = function () {
+  var resetBtn = document.createElement('button');
+  resetBtn.setAttribute('id', 'reset');
+  resetBtn.addEventListener('click', reset);
+  resetBtn.innerHTML = "Reset Board";
+  gameboard.appendChild(resetBtn);
+};
+
+var reset = function () {
+  var gamereset = document.getElementById('game-board');
+  for (var i = gamereset.children.length; i > 0; i--) {
+      gamereset.removeChild(gamereset.children[0]);
+    }
+  cardsInPlay = [];
+  createCards();
+};
 createCards();
