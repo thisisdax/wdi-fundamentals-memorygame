25517b1ee86eda66933f22d2de28d2059dad5e53
Dax <Dax@Pokedax.local>
2016-12-02 16:55:28 +0800

    unit 11 assignment - to be improved

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,5 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-
+var cards = ['Queen', 'Queen', 'King', 'King'];
+var cardsInPlay = [];
 /* if (cardOne === cardTwo) {
   alert("You found a match!");
 } else {
@@ -12,11 +9,35 @@ var cardFour = "King";
 var gameboard = document.getElementById('game-board');
 
 var createCards = function () {
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < cards.length; i++) {
     var createDiv = document.createElement('div');
     createDiv.className = 'cards';
-    gameboard.appendChild(createDiv)
+    createDiv.setAttribute('data-card', cards[i]);
+    createDiv.addEventListener('click', isTwoCards);
+    gameboard.appendChild(createDiv);
   }
+};
+
+var isTwoCards = function () {
+    if (this.getAttribute('data-card') === 'King') {
+    this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/52/spades-28320_960_720.png' alt='King of Spades' class='cards') />";
+    } else {
+    this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2012/04/11/13/48/hearts-28307_960_720.png' alt='Queen of Hearts' class='cards') />";
+    }
+    cardsInPlay.push(this.getAttribute('data-card'));
+    if (cardsInPlay.length === 2) {
+      isMatch(cardsInPlay);
+      cardsInPlay = [];
+    }
+};
+
+var isMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again!");
+  }
+  cardsInPlay
 };
 
 createCards();
